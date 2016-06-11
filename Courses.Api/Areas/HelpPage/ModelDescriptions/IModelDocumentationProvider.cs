ef2578228fdf96badf32616d938bab5b5bdcf339
moahelmy <muhammad.cs@gmail.com>
2016-06-11 14:35:55 +1000

    Adding the web api. It is empty for now

---
+++ Courses.Api/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace Courses.Api.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
