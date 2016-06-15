8cba160ecff8e20d1455fd56db53ea76b2316965
moahelmy <muhammad.cs@gmail.com>
2016-06-15 23:00:13 +1000

    Add view to read web service url from app settings.

---
+++ Courses.Web/Controllers/SettingsController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace Courses.Web.Controllers
+{
+    public class SettingsController : Controller
+    {
+        // GET: Settings
+        public ActionResult Index()
+        {
+            ViewBag.apiURL = Properties.Settings.Default.ApiUrl;
+            return View();
+        }
+    }
+}
---
+++ Courses.Web/Global.asax.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Web;
+using System.Web.Routing;
+
+namespace Courses.Web
+{
+    public class Global : HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }        
+    }
+}
